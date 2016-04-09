90d865e49ea51111c1e737ff89bf3900f6a5cbda
thanawatnew <thanawatnew@gmail.com>
2016-04-09 16:38:59 +0700

    change the structure of locations dictionary to have more depth

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 <script src="data.json"></script>
 <script>
-var data =[];
+//var data =[];
 function getData() {
-	var data2;
+	//var data2;
     $.ajax({
         url: "/html/wimb/buses.json",//"/Home/GetData/",
         type: "GET",
@@ -15,11 +15,29 @@ function getData() {
             //console.log(data.key1); // value for key1
             //or to list all values
             for(var key in data){
-		 locations[data[key][0]][8][data[key][1]+'_'+data[key][2]+'_'+data[key][3]+'_'+data[key][4]]=data[key][5]
-                 console.log(data[key]);
+		 if(!(data[key][0] in locations))
+		 {
+			console.log("error: not found cam_id = "+data[key][0]);
+			//locations[data[key]]={};
+			 //locations[data[key][0]][8][data[key][1]+'_'+data[key][2]]={data[key][3]:{data[key][4]:data[key][5]}};
+
+		 }
+		 else if(!(data[key][1]+'_'+data[key][2] in locations[data[key][0]][8]))
+		 {
+			locations[data[key][0]][8][data[key][1]+'_'+data[key][2]]={};
+			locations[data[key][0]][8][data[key][1]+'_'+data[key][2]][data[key][3]]={};
+			locations[data[key][0]][8][data[key][1]+'_'+data[key][2]][data[key][3]][data[key][4]]=data[key][5];
+		 }
+		 else if(!(data[key][3] in locations[data[key][0]][8][data[key][1]+'_'+data[key][2]]))
+		 {
+			 locations[data[key][0]][8][data[key][1]+'_'+data[key][2]][data[key][3]]={};
+			locations[data[key][0]][8][data[key][1]+'_'+data[key][2]][data[key][3]][data[key][4]]=data[key][5];
+		 }
+		 else locations[data[key][0]][8][data[key][1]+'_'+data[key][2]][data[key][3]][data[key][4]]=data[key][5];
+                 //console.log(data[key]);
             }
-		data2=data;
-		console.log('test');
+		//data2=data;
+		console.log('downloaded data');
 		 setTimeout(getData, 5000);	
         },
 	error: function(xhr,status,error){
