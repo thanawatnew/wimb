39828c541b5a32e140a76a30ac88d4ad4cc7e36f
thanawatnew <thanawatnew@gmail.com>
2016-04-10 01:53:31 +0700

    able to view images from haar,ocr from infowindow in the website

--- index.php
+++ index.php
@@ -1,11 +1,54 @@
 <html>
 <head>
+<style>
+#listContainer{
+  margin-top:15px;
+}
+ 
+#expList ul, li {
+    list-style: none;
+    margin:0;
+    padding:0;
+    cursor: pointer;
+}
+#expList p {
+    margin:0;
+    display:block;
+}
+#expList p:hover {
+    background-color:#121212;
+}
+#expList li {
+    line-height:140%;
+    text-indent:0px;
+    background-position: 1px 8px;
+    padding-left: 20px;
+    background-repeat: no-repeat;
+}
+</style>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 <script src="data.json"></script>
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?v=3.6"></script>
 <script src="/html/wimb/js/maps.js" type="text/javascript"></script>
 <script>
 //var data =[];
+function setFlipbook(id,type,image_path)
+{
+	if(type=="normal")
+	content='<iframe src="http://www.bmatraffic.com/PlayVideo.aspx?ID='+id+'" scrolling="no" frameborder="0" \
+		style="width: 400px; height: 266px; display: block; padding: 0px; margin-top:0px;"></iframe>\
+		<img src="http://www.bmatraffic.com/images/logo-bkk-small.png">\
+		';
+	else if(type=="big")
+		content = '<img src="/html/wimb/images/'+image_path+'.jpg" width="400px" height="266px">';
+	else if(type=="haar")
+		content = '<img src="/html/wimb/images_haar/'+image_path+'.jpg" width="400px" height="266px">';
+	else if(type=="ocr")
+		content = '<img src="/html/wimb/images_number/'+image_path+'.jpg" width="400px" height="266px">';
+	$('#flipbook-'+id).html(content);
+		
+}
+
 function getData() {
 	//var data2;
     $.ajax({
---
+++ js/maps.js
@@ -0,0 +1,76 @@
+function prepareList() {
+  $('#expList').find('li:has(ul)')
+  	.click( function(event) {
+  		if (this == event.target) {
+  			$(this).toggleClass('expanded');
+  			$(this).children('ul').toggle('medium');
+  		}
+  		return false;
+  	})
+  	.addClass('collapsed')
+  	.children('ul').hide();
+  };
+ 
+  $(document).ready( function() {
+      prepareList();
+  });
+function initialize()
+{
+	var map = 
+	    new google.maps.Map(document.getElementById('map-canvas'));
+	var bounds = new google.maps.LatLngBounds();
+	var infowindow = new google.maps.InfoWindow();
+	var count=0;
+	for (var i in locations)
+	{
+		var p = locations[i];
+		if(p[5]==0 && p[6]==0) continue;
+		if(Object.keys(locations[i][8]).length<=0) continue;
+		var latlng = new google.maps.LatLng(p[5], p[6]);
+		bounds.extend(latlng);
+		content='<div class="popup-img" id="flipbook-'+i+'">\
+		<iframe src="http://www.bmatraffic.com/PlayVideo.aspx?ID='+p[0]+'" scrolling="no" frameborder="0" \
+		style="width: 400px; height: 266px; display: block; padding: 0px; margin-top:0px;"></iframe>\
+		<img src="http://www.bmatraffic.com/images/logo-bkk-small.png">\
+		</div>';
+		content+='<br><a href="#" onclick="setFlipbook('+i+",'normal','0'"+')">normal</a>';
+		
+		content+='<br><div id="listContainer"><ul id="expList">';
+		for(var j in p[8])
+		{
+			
+    
+			//content+='<li><a href="#" class="hide" data-toggle="#list">'+j+'</a><ul class="out" id="list">';
+			content+='<li><a href="#" onclick="'+"setFlipbook("+i+",'big','"+i+'_'+j+"')"+'"'+">"+j+'<ul>';
+			for(var k in p[8][j])
+			{
+				
+				content+='<li><a href="#" onclick="'+"setFlipbook("+i+",'haar','"+i+'_'+j+'_result_'+k+"')"+'"'+">"+j+'<ul>';
+				for(var l in p[8][j][k])
+					content+='<li><a href="#" onclick="'+"setFlipbook("+i+",'ocr','"+i+'_'+j+'_result_'+k+'_result_number_'+l+"')"+'"'+">"+j+'_'+k+'_'+l+': '+p[8][j][k][l]+'<ul>';
+				 
+			}
+		}
+		
+        content+='</ul></li></ul></div>';
+		var marker = new google.maps.Marker({
+			position: latlng,
+			map: map,
+			title: p[0]+' '+p[2],
+			content: content
+		});
+	
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.setContent(this.title+'<br>'+this.content);
+			infowindow.open(map, this);
+			map.setCenter(marker.getPosition());
+			
+		});
+		
+	}
+	
+	map.fitBounds(bounds);
+	
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
