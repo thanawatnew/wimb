aca9f0f56efab25be7f9bd1acd1c8bf5d7507866
thanawatnew <thanawatnew@gmail.com>
2016-05-06 11:41:54 +0700

    add test from images dataset to detect_bus_haar

--- detect_bus_haar.py
+++ detect_bus_haar.py
@@ -3,6 +3,8 @@ import os
 from subprocess import call
 import subprocess
 import detect_bus_number
+import shutil
+#from start_wimb import check_init_files_and_folders
 path=os.path.abspath('.')
 file_type='.jpg'
 is_ocr=False
@@ -74,10 +76,64 @@ def detect_bus_haar(path,file_name,file_type,is_ocr,cascade):
 	 #cv2.imshow("frame", img)
 def detect_bus_haar_group(path):
 	count=0#0+215-1
-	cascade = cv2.CascadeClassifier(path+"/models/cascade_wimb_bus_front_33_stages_1000_pos_3000_neg_wrong.xml")#"/models/cascade_wimb_bus_front_100_stages_1000_pos_3000_neg.xml") #"/models/cascade_wimb_bus_front_33_stages_1000_pos_3000_neg_wrong.xml")#"cascade_wimb_bus_front_100_stages_1000_pos_3000_neg.xml")#/cascade_front_30_stage_wrong.xml")#cascade_30_stages_1000_pos_3000_neg.xml")#cascade_front_30_stage_wrong.xml") #"cascade_front_19_stage_1000_pos_3000_neg.xml")#"/cascade_front_30_stage_wrong.xml")
+	cascade = cv2.CascadeClassifier(path+"/models/cascade_wimb_bus_front_33_stages_1000_pos_3000_neg_wrong.xml")#"/models/cascade_bus_front_130_stages_1000_pos_3000_neg.xml")#"/models/cascade_wimb_bus_front_33_stages_1000_pos_3000_neg_wrong.xml")#"/models/cascade_wimb_bus_front_100_stages_1000_pos_3000_neg.xml") #"/models/cascade_wimb_bus_front_33_stages_1000_pos_3000_neg_wrong.xml")#"cascade_wimb_bus_front_100_stages_1000_pos_3000_neg.xml")#/cascade_front_30_stage_wrong.xml")#cascade_30_stages_1000_pos_3000_neg.xml")#cascade_front_30_stage_wrong.xml") #"cascade_front_19_stage_1000_pos_3000_neg.xml")#"/cascade_front_30_stage_wrong.xml")
 	for file_name_with_extension in os.listdir(path+'/images/')[count:]:
 	 count+=1
 	 if is_debug: print 'progress = '+str(count)+'/'+str(len(os.listdir(path+'/images/')))+' '+str(count*100.0/len(os.listdir(path+'/images/')))
 	 if is_debug: print 'file name = '+file_name_with_extension
 	 file_name,file_type=file_name_with_extension.split('.')[0],'.'+file_name_with_extension.split('.')[1]
-	 detect_bus_haar.detect_bus_haar(path,file_name,file_type,False,cascade)
+	 detect_bus_haar(path,file_name,file_type,False,cascade)
+
+def check_init_files_and_folders():
+	"""
+	check if file exist
+	>>> check_init_files_and_folders() 
+	file models/cascade_wimb_bus_front_33_stages_1000_pos_3000_neg_wrong.xml existed: True
+	file detect_bus_haar_group.py existed: True
+	file get_cam_detail.py existed: True
+	file get_image.py existed: True
+	file start_wimb.py existed: True
+	directory images existed: True
+	directory images_haar existed: True
+	directory images_number existed: True
+	directory models existed: True
+	"""
+	#['cascade_wimb_bus_front_100_stages_1000_pos_3000_neg.xml', 'cascade_wimb_bus_front_33_stages_1000_pos_3000_neg_wrong.xml', 'color_detect_2.py', 'dedupe.py', 'detect_image_group_ku.py', 'detect_shape_5.py', 'get_cam_id_2.py', 'get_image_8.py', 'gui_hsv.py', 'knaps.py', 'knapsack_2.py', 'maps.html', 'program_detect_rectangle.zip', 'start_capture.py']
+	file_list=[
+	#'cascade_wimb_bus_front_100_stages_1000_pos_3000_neg.xml', 
+	'models/cascade_wimb_bus_front_33_stages_1000_pos_3000_neg_wrong.xml', 
+	#'color_detect_2.py', 
+	#'dedupe.py', 
+	'detect_bus_haar_group.py', 
+	#'detect_shape_5.py', 
+	'get_cam_detail.py', 
+	'get_image.py', 
+	#'gui_hsv.py', 
+	#'knaps.py', 
+	#'knapsack_2.py', 
+	#'maps.html', 
+	#'program_detect_rectangle.zip', 
+	'start_wimb.py',
+	'g.php',
+	]
+	directory_list=[
+	'images',
+	'images_haar',
+	'images_haar_result',
+	'images_number',
+	'images_number_result',
+	'models',
+	'images_old',
+	'text_number',
+	]
+	
+	for file_name in file_list: print 'file '+file_name+' existed: '+str(os.path.isfile(file_name))
+	for directory_name in directory_list: 
+		print 'directory '+directory_name+' existed: '+str(os.path.isdir(directory_name))
+		if not os.path.isdir(directory_name): 
+			os.makedirs(directory_name)
+		if "images" in directory_name: shutil.copy(path+'/g.php',path+'/'+directory_name+'/g.php')
+		
+if __name__ == "__main__":
+	check_init_files_and_folders()
+	detect_bus_haar_group(path)
