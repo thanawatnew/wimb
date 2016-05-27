a39deb889921d5647019ea2b416cbe73c52ac913
thanawatnew <thanawatnew@gmail.com>
2016-05-27 22:32:21 +0700

    add for to load more picture and support bgs as needed

--- get_image.py
+++ get_image.py
@@ -50,32 +50,36 @@ def get_pictures(cookies):
 	
 	sessionID=cookies.split("=")[1]
 	for id in list_cam_id:
-		id=str(id)		
-		try: send_response(url+"PlayVideo.aspx?ID="+id,cookies)
-		except: 
-			is_working=False
-			while not is_working:
-				try: 
-					cookies=get_cookie(url)
-					is_working=True
-				except: pass
-			send_response(url+"PlayVideo.aspx?ID="+id,cookies)
-		try: content=send_response(url+"show.aspx?image="+id,cookies)
-		except: 
-			is_working=False
-			while not is_working:
-				try: 
-					cookies=get_cookie(url)
-					is_working=True
-				except: pass
-			content=send_response(url+"show.aspx?image="+id,cookies)
-		
-		timestr = time.strftime("%Y%m%d_%H%M%S")
-		f=open(path+'/images/'+id+'_'+timestr+'.jpg','wb')
-		f.write(content)
-		f.close()
-		file_name= id+'_'+timestr#+'.jpg'
-		cascade = cv2.CascadeClassifier(path+"/models/cascade_wimb_bus_front_33_stages_1000_pos_3000_neg_wrong.xml")
+		id=str(id)	
+		#file_name_list = []
+		for round in xrange(1): #collect three pictures from BMATRAFFIC
+			try: send_response(url+"PlayVideo.aspx?ID="+id,cookies)
+			except: 
+				is_working=False
+				while not is_working:
+					try: 
+						cookies=get_cookie(url)
+						is_working=True
+					except: pass
+				send_response(url+"PlayVideo.aspx?ID="+id,cookies)
+			try: content=send_response(url+"show.aspx?image="+id,cookies)
+			except: 
+				is_working=False
+				while not is_working:
+					try: 
+						cookies=get_cookie(url)
+						is_working=True
+					except: pass
+				content=send_response(url+"show.aspx?image="+id,cookies)
+				if len(content)<50: continue	
+			timestr = time.strftime("%Y%m%d_%H%M%S")
+			f=open(path+'/images/'+id+'_'+timestr+'.jpg','wb')
+			f.write(content)
+			f.close()
+			file_name= id+'_'+timestr#+'.jpg'
+			#file_name_list.append(file_name)
+			#time.sleep(1) # delay 1.5 second each time before collect a picture
+		cascade = cv2.CascadeClassifier(path+"/models/cascade_bus_front_175_stages_1000_pos_3000_neg.xml")#"/models/cascade_wimb_bus_front_33_stages_1000_pos_3000_neg_wrong.xml")
 		rects=detect_bus_haar.detect_bus_haar(path,file_name,'.jpg',False,cascade)
 
 		if is_debug: print "==================================================================================="
